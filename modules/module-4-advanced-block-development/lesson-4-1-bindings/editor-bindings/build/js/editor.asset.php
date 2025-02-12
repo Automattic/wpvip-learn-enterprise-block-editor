6e73774be83efb6a81af9ec556d32dc79ed6b5bc
JuanMa Garrido <juanma.garrido@gmail.com>
2025-02-12 17:31:02 +0000

    block bindings examples

---
+++ modules/module-4-advanced-block-development/lesson-4-1-bindings/editor-bindings/build/js/editor.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'ff65ec9677a88971ab30');
---
+++ modules/module-4-advanced-block-development/lesson-4-1-bindings/editor-bindings/webpack.config.js
@@ -0,0 +1,20 @@
+// WordPress webpack config.
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+
+// Utilities.
+const path = require( 'path' );
+
+// Add any a new entry point by extending the webpack config.
+
+module.exports = {
+	...defaultConfig,
+	...{
+		entry: {
+			'js/editor': path.resolve(
+				process.cwd(),
+				'resources/js',
+				'editor.js'
+			),
+		},
+	},
+};
