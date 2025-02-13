67331646ca520039c40ba4d057ab36b0ad5e2437
JuanMa Garrido <juanma.garrido@gmail.com>
2025-02-13 17:29:56 +0000

    context example

---
+++ examples/context/build/record-title/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-blocks'), 'version' => 'dfc852cf08dc372e2000');
---
+++ examples/context/build/record/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '989570b65606cb73cb3d');
