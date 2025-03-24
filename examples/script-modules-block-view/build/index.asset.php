1dc6125ad6408adaeaa50c67dcfa61e3749d83c1
JuanMa <juanma.garrido@gmail.com>
2025-03-24 10:29:02 +0000

    Update asset versions and refactor view.js for improved module loading
    - Updated version numbers in index.asset.php and view.asset.php to reflect recent changes.
    - Refactored view.js to enhance module loading and error handling, utilizing async/await for better performance.
    - Streamlined the overall structure of the code for improved readability and maintainability.

--- examples/script-modules-block-view/build/index.asset.php
+++ examples/script-modules-block-view/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '0edd2b85b292ce0ebe22');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '47feba49d7016422cf42');
