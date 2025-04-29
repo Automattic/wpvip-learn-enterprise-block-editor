7df35ac9c80d71fbe2d34673dc0e64b264da749a
JuanMa <juanma.garrido@gmail.com>
2025-04-29 18:17:02 +0200

    Refactor and replace WpOrg_Plugin_Query with a new block registration
    - Removed the obsolete `WpOrg_Plugin_Query` class and its associated file, streamlining the codebase.
    - Updated the plugin file to register a new block for retrieving plugin information from WordPress.org, enhancing the integration with Remote Data Blocks.
    - Defined a new input and output schema for the block, ensuring compatibility with the expected data structure from the API.

--- examples/remote-data-blocks-custom-query/plugin.php
+++ examples/remote-data-blocks-custom-query/plugin.php
@@ -1,34 +1,111 @@
 <?php
 /**
- * Plugin Name:       My Theme - WP.org Plugin Info Query
- * Description:       Registers a custom Remote Data Blocks query to fetch WordPress.org plugin information by slug.
- * Version:           1.0.0
- * Requires at least: 6.4
- * Requires PHP:      7.4
- * Author:            My Theme Author
- * Author URI:        https://author.example.com/
- * License:           GPL v2 or later
- * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       my-theme
- * Domain Path:       /languages
- * Requires Plugins:  remote-data-blocks
+ * Plugin Name: WP.org Plugin Info RDB Example
+ * Description: Creates a custom block to be used with Remote Data Blocks in order to retrieve plugin information from WordPress.org.
+ * Author: WPVIP
+ * Author URI: https://remotedatablocks.com/
+ * Text Domain: remote-data-blocks
+ * Version: 1.0.0
+ * Requires Plugins: remote-data-blocks
  *
- * @package           MyTheme
+ * @package RemoteDataBlocks
  */
 
-namespace MyTheme\RemoteData;
+namespace RemoteDataBlocks\Example\WpOrgPlugin;
 
-use Automattic\Remote_Data_Blocks\Registry\Query as Query_Registry;
+use RemoteDataBlocks\Config\DataSource\HttpDataSource;
+use RemoteDataBlocks\Config\Query\HttpQuery;
 
-// Include the WpOrg_Plugin_Query class.
-require_once __DIR__ . '/class-wporg-plugin-query.php';
+define( 'EXAMPLE_WPORG_PLUGIN_DATA_SOURCE_UUID', 'ee0dbb0b-ebe5-4097-8cdd-bcd2e5d02b95' );
 
 /**
- * Register the custom WP.org Plugin Query with the Remote Data Blocks registry.
+ * Registers a custom block to retrieve plugin information from WordPress.org.
  *
- * @param Query_Registry $registry The Query Registry instance.
+ * @return void
  */
-function register_wporg_plugin_query( $registry ) {
-	$registry->register( new WpOrg_Plugin_Query() );
+function register_wporg_plugin_block() {
+	if ( ! defined( 'EXAMPLE_WPORG_PLUGIN_DATA_SOURCE_UUID' ) ) {
+		return;
+	}
+
+	$plugin_data_source = HttpDataSource::from_uuid( EXAMPLE_WPORG_PLUGIN_DATA_SOURCE_UUID );
+
+	if ( ! $plugin_data_source instanceof HttpDataSource ) {
+		return;
+	}
+
+	$plugin_query = HttpQuery::from_array(
+		array(
+			'data_source'   => $plugin_data_source,
+			'endpoint'      => function ( $input_variables ) use ( $plugin_data_source ) {
+				return $plugin_data_source->get_endpoint() . '/plugins/info/1.0/' . $input_variables['plugin_slug'] . '.json';
+			},
+			'input_schema'  => array(
+				'plugin_slug' => array(
+					'name' => 'Plugin Slug',
+					'type' => 'string',
+				),
+			),
+			'output_schema' => array(
+				'is_collection' => false,
+				'type'          => array(
+					'name'              => array(
+						'name' => 'Plugin Name',
+						'path' => '$.name',
+						'type' => 'string',
+					),
+					'slug'              => array(
+						'name' => 'Plugin Slug',
+						'path' => '$.slug',
+						'type' => 'string',
+					),
+					'version'           => array(
+						'name' => 'Current Version',
+						'path' => '$.version',
+						'type' => 'string',
+					),
+					'author'            => array(
+						'name' => 'Plugin Author',
+						'path' => '$.author',
+						'type' => 'string',
+					),
+					'rating'            => array(
+						'name' => 'Average Rating',
+						'path' => '$.rating',
+						'type' => 'number',
+					),
+					'num_ratings'       => array(
+						'name' => 'Number of Ratings',
+						'path' => '$.num_ratings',
+						'type' => 'integer',
+					),
+					'downloaded'        => array(
+						'name' => 'Download Count',
+						'path' => '$.downloaded',
+						'type' => 'integer',
+					),
+					'last_updated'      => array(
+						'name' => 'Last Updated',
+						'path' => '$.last_updated',
+						'type' => 'string',
+					),
+					'short_description' => array(
+						'name' => 'Short Description',
+						'path' => '$.short_description',
+						'type' => 'string',
+					),
+				),
+			),
+		)
+	);
+
+	register_remote_data_block(
+		array(
+			'title'        => 'WP.org Plugin Info',
+			'render_query' => array(
+				'query' => $plugin_query,
+			),
+		)
+	);
 }
-add_action( 'remote_data_blocks_query_registry_init', __NAMESPACE__ . '\register_wporg_plugin_query' );
+add_action( 'init', __NAMESPACE__ . '\\register_wporg_plugin_block' );
